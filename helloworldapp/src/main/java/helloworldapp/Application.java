dc3ee32a4b96fabd05f870befaf9dc264d082da6
AlperMulayim <alper.mulayim48@gmail.com>
2021-02-13 16:52:54 +0300

    hello world app init

---
+++ helloworldapp/src/main/java/helloworldapp/Application.java
@@ -0,0 +1,10 @@
+package helloworldapp;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
